df6e4c5df21482357c59cbcdbaa31badb0f2a6bb
Alex Duan <40921264+The1AndOnlyAlex@users.noreply.github.com>
2022-02-15 22:27:44 -0800

    Initialized first files and game board
    2/15/2022

---
+++ turn.java
@@ -0,0 +1,12 @@
+public class turn {
+    
+    public static int Taketurn(int turnNum) {
+
+        if(turnNum == 1) {
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
+}
